e04e4670675debf2a8c9d52c8333e964362d9e7b
camilofdf123-svg <camilofdf123@gmail.com>
2026-06-03 12:10:20 -0500

    Add functions to return messages and full names

---
+++ Funcionesconreturn.py
@@ -0,0 +1,13 @@
+def obtener_mensaje():
+    mensaje = 'Bienvenido al sistema'
+    return mensaje
+
+def generar_nombre_completo():
+    nombre = 'Santiago'
+    apellido = 'Moreno'
+    espacio = ' '
+    nombre_completo = nombre + espacio + apellido
+    return nombre_completo
+
+print(obtener_mensaje())
+print(generar_nombre_completo())
